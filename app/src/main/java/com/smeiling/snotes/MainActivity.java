6796339513b3042ca6b33f48f271c7e743715471
songmeiling <songmeiling@SMLSMLSML.local>
2017-08-07 17:54:11 +0800

    init snotes project

---
+++ app/src/main/java/com/smeiling/snotes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smeiling.snotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
